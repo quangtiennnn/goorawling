f743ab7cf0c25ff10faf70ffa9505610817d8430
Quang Tien Nguyen <105355188+quangtiennnn@users.noreply.github.com>
2025-06-05 12:11:07 +0700

    add-code again?

--- tools.py
+++ tools.py
@@ -20,7 +20,7 @@ import pandas as pd
 import numpy as np
 from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
 import random
-
+import json
 #====================== CHROME DRIVER SETUP ======================
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
@@ -37,51 +37,7 @@ chrome_options.add_argument("--disable-extensions")
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
 
-#====================== LINK CRAWLING ======================
-def googlemapCrawl(district, city = 'Thành phố Hồ Chí Minh'):
-    kw_input = f"Nhà hàng ở {district}, {city}"
-    #====================== SEARCH ====================================
-    driver = webdriver.Chrome(options=chrome_options)
-    path_search = "https://www.google.com/maps?hl=vi-VN"
-    driver.get(path_search)
-    input_search = driver.find_element(By.ID, "searchboxinput")
-    input_search.send_keys(kw_input)
-    input_search.send_keys(Keys.ENTER)
-    sleep(5)
-    #====================== SCROLL DOWN ===============================
-    start_time = time.time()
-    
-    scroll_items = driver.find_elements(By.CLASS_NAME, "hfpxzc")
-    while True:
-      try:
-        element = driver.find_element(By.CSS_SELECTOR, 'span.HlvSq')
-        break
-      except NoSuchElementException:
-        scroll_items[1].send_keys(Keys.DOWN)
-        # Timeout handling
-        elapsed_time = time.time() - start_time
-        if elapsed_time > 300:
-            print("Timeout exceeded. Exiting loop.")
-            break
-    #====================== HTML CONTENTS ===============================
-    html_content = driver.page_source
-    driver.quit()
-    soup = BeautifulSoup(html_content, 'html.parser')
-    elements = soup.find_all(class_='hfpxzc')
-    
-    if not os.path.exists('data_crawl_link'):
-          os.makedirs(directory_name)
-    filename = f'data_crawl_link/{unidecode(str(district).lower())}_{unidecode(city.lower())}.txt'
-    with open(filename, 'w', encoding='utf-8') as output_file:
-        # Extract and write the href attributes to the file
-        for element in elements:
-            href = element.get('href')
-            if href:
-                output_file.write(href + '\n')
-    output = f"Saved to {filename}"
-    print(output)
-
-
+#====================== DATA COLLECTION ======================
 def data_collection(elem):
     try:
         name = elem.find(class_='d4r55').text
@@ -107,7 +63,7 @@ def data_collection(elem):
         review_rating = elem.find('span', class_='kvMYJc').get('aria-label')
     except:
         review_rating = ''
-        # Extract image URLs
+    # Extract image URLs
     images = []
     try:
         image_buttons = elem.find_all('button', class_='Tya61d')
@@ -124,11 +80,47 @@ def data_collection(elem):
     return [name, infos, collected_date, review, categorize, review_rating, images]
 
 
-#====================== REVIEW CRAWLING ======================
-def getReviews(driver,number_of_reviews,file_path,constant = 0.55,thresh_hold = 1000):
+#====================== LANGUAGES ======================
+def change_language_to_english(url):
+    # Parse the URL into components
+    parsed_url = urlparse(url)
     
+    # Parse the query parameters into a dictionary
+    query_params = parse_qs(parsed_url.query)
+    
+    # Change the 'hl' parameter to 'en'
+    query_params['hl'] = 'en'
+    
+    # Reconstruct the query string
+    new_query = urlencode(query_params, doseq=True)
+    
+    # Reconstruct the full URL with the updated query string
+    new_url = urlunparse(parsed_url._replace(query=new_query))
+    
+    return new_url
+
+def change_language_to_vietnamese(url):
+    # Parse the URL into components
+    parsed_url = urlparse(url)
+    
+    # Parse the query parameters into a dictionary
+    query_params = parse_qs(parsed_url.query)
+    
+    # Change the 'hl' parameter to 'en'
+    query_params['hl'] = 'vi'
+    
+    # Reconstruct the query string
+    new_query = urlencode(query_params, doseq=True)
+    
+    # Reconstruct the full URL with the updated query string
+    new_url = urlunparse(parsed_url._replace(query=new_query))
+    
+    return new_url
+
+#====================== REVIEW CRAWLING ======================
+def get_reviews(driver,number_of_reviews,file_path,constant = 0.55,thresh_hold = 1000):
     #====================== LIMIT THE REVIEW ===============================
-    if number_of_reviews > 1000:
+    if number_of_reviews > thresh_hold:
         number_of_reviews = thresh_hold
     
     #====================== SCROLL ===============================
@@ -164,9 +156,8 @@ def getReviews(driver,number_of_reviews,file_path,constant = 0.55,thresh_hold = 
     for elem in elems:
         new_data = data_collection(elem)
         data.append(new_data)
-
         # Print progress
-        print(f"{file_path} {i}/{number_of_reviews}")
+        print(f"{file_path}{elem}/{number_of_reviews}")
     
     #====================== COLLECTING DATA =================================
     header = ['name','infos','collected_date','review','categorize','review_rating']
@@ -183,21 +174,83 @@ def getReviews(driver,number_of_reviews,file_path,constant = 0.55,thresh_hold = 
     except:
         pass
         
-def reviewCrawl(link,folder_name = 'sample'):
-    #====================== NAME =================================
-    # coordinates = link.split('/')[6].split("=")[1].split("!")[5:7]
-    coordinates = link.split('/')[6].split("@")[1].split(",")[:2]
 
-    name = f"{coordinates[0][2:]},{coordinates[1][2:]}"
+#====================== IMAGE CRAWLING ======================
+def move_to_all_photos(driver):
+    driver.find_element(By.XPATH, '//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[26]/div[2]/div[2]/div[2]/div[1]/button').click()
+
+def tag_click(driver,tag):
+    driver.find_element(By.XPATH, f'//div[contains(@class, "Gpq6kf") and contains(@class, "NlVald") and text()="{tag}"]').click()
+
+def get_images(driver,file_path):
+    restaurant_id = file_path.split("/")[-1].replace(".csv", "")
+    # Get all tag names except 'All' and 'Latest' :)
+    elems = driver.find_elements(By.CSS_SELECTOR, '.Gpq6kf.NlVald')
+    exclude_tags = {'Tất cả', 'Mới nhất', 'All', 'Latest','Video'}
+    tag_names = [elem.text for elem in elems if elem.text.strip().lower() not in exclude_tags and elem.text.strip()]
+
+    tag_images = {}
+    for tag in tag_names:
+        try:
+            tag_click(driver,tag)
+            time.sleep(random.uniform(1,2))  # Wait for images to load after clicking tag
+            image_loaded = driver.find_elements(By.CSS_SELECTOR, '.Uf0tqf.loaded')
+            urls = []
+            for image in image_loaded:
+                style = image.get_attribute('style')
+                match = re.search(r'url\((.*?)\)', style)
+                if match:
+                    url = match.group(1).replace('&quot;', '').strip('"')
+                    urls.append(url)
+            tag_images[tag] = urls
+        except Exception:
+            tag_images[tag] = []
+            
+    # Prepare directory
+    if len(file_path.split('/')) > 1:
+        directory = '/'.join(file_path.split('/')[:-1])
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+    # Write to CSV: columns = restaurant_id, images (as JSON)
+    try:
+        with open(file_path, mode='w', newline='', encoding='utf-8-sig') as file:
+            csv_writer = csv.writer(file)
+            csv_writer.writerow(['restaurant_id', 'images'])
+            csv_writer.writerow([restaurant_id, tag_images])
+    except:
+        pass
+
+
+#====================== REVIEW CRAWLING ======================
+def google_crawl(restaurant_id: str, link, folder_name: str = 'sample'):
+    images_path = f'{folder_name}/images/{restaurant_id}.csv'
+    reviews_path = f'{folder_name}/reviews/{restaurant_id}.csv'
     #====================== driver SETTINGS =====================
     driver = webdriver.Chrome(options=chrome_options)
     actions = ActionChains(driver)
     driver.get(link)
+    
+    ## Click on the "All" button to show all photos
+    actions = ActionChains(driver)
+    all_button = driver.find_element(By.XPATH, '//button[contains(@class, "K4UgGe") and @aria-label="Tất cả"]')
+    
+    actions.move_to_element(all_button).perform() # Move to the button and click it!!!
+    time.sleep(random.uniform(1, 2))
+    all_button.click()
+    time.sleep(random.uniform(1, 2))
+    get_images(driver,images_path)
+
+    #====================== REFRESH =====================
+    driver.get(link)
+    time.sleep(random.uniform(3, 5))
     #=============================================================
     driver.find_element(By.XPATH, '//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[3]/div/div/button[2]').click()
+    tag_click(driver,'Bài đánh giá')
+
     time.sleep(2)
     driver.find_element(By.CSS_SELECTOR, ".HQzyZ").click()
-    # driver.find_element(By.CSS_SELECTOR, ".DVeyrd").click()
+    # driver.find_element(By.CSS_SELECTOR, ".DVeyrd").click()       
     time.sleep(2)
     actions.send_keys(Keys.DOWN).perform()
     time.sleep(0.5)
@@ -207,45 +260,5 @@ def reviewCrawl(link,folder_name = 'sample'):
     number_of_reviews = int(driver.find_element(By.XPATH, '//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/div/div[2]/div[3]').text.split()[0].replace('.', ''))
     
     time.sleep(2)
-
-    file_path = f'{"data_review_crawl"}/{folder_name}/{name}.csv'
-
-    getReviews(driver,number_of_reviews,file_path)
-
-
-def change_language_to_english(url):
-    # Parse the URL into components
-    parsed_url = urlparse(url)
-    
-    # Parse the query parameters into a dictionary
-    query_params = parse_qs(parsed_url.query)
-    
-    # Change the 'hl' parameter to 'en'
-    query_params['hl'] = 'en'
-    
-    # Reconstruct the query string
-    new_query = urlencode(query_params, doseq=True)
-    
-    # Reconstruct the full URL with the updated query string
-    new_url = urlunparse(parsed_url._replace(query=new_query))
-    
-    return new_url
-
-
-def change_language_to_vietnamese(url):
-    # Parse the URL into components
-    parsed_url = urlparse(url)
-    
-    # Parse the query parameters into a dictionary
-    query_params = parse_qs(parsed_url.query)
-    
-    # Change the 'hl' parameter to 'en'
-    query_params['hl'] = 'vi'
-    
-    # Reconstruct the query string
-    new_query = urlencode(query_params, doseq=True)
-    
-    # Reconstruct the full URL with the updated query string
-    new_url = urlunparse(parsed_url._replace(query=new_query))
-    
-    return new_url
+    time.sleep(random.uniform(3, 5))
+    get_reviews(driver,number_of_reviews,reviews_path)
